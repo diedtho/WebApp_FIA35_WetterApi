554fdc8c98245ac18d23f9b201037111a5001320
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-11-17 13:30:29 +0100

    Projektdateien hinzufügen.

---
+++ WebApp_FIA35_WetterApi/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace WebApp_FIA35_WetterApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Wetter(string Ort)
+        {
+            Models.Wetter wetter = new Models.Wetter
+            {
+                Ort = "Unbekannt"
+            };
+
+            if (Ort == "Oberhausen")
+            {
+                wetter.Ort = "Oberhausen";
+                wetter.Temperatur = new Random().Next(5, 15);
+                wetter.Luftdruck = new Random().Next(950, 1050);
+            }
+
+            if (Ort == "Neuss")
+            {
+                wetter.Ort = "Neuss";
+                wetter.Temperatur = new Random().Next(5, 15);
+                wetter.Luftdruck = new Random().Next(950, 1050);
+            }
+
+            if (Ort == "Nienborg")
+            {
+                wetter.Ort = "Nienborg";
+                wetter.Temperatur = new Random().Next(5, 15);
+                wetter.Luftdruck = new Random().Next(950, 1050);
+            }
+
+
+            return Json(wetter, new JsonSerializerOptions { WriteIndented = true });
+        }
+
+
+        // Ohne Funktion, nur Demo
+        [HttpPost]
+        public IActionResult Wetter()
+        {
+            return Ok(); // Status-Code 200
+        }
+
+
+}
---
+++ WebApp_FIA35_WetterApi/Models/Wetter.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp_FIA35_WetterApi.Models
+{
+    public class Wetter
+    {
+        public string Ort { get; set; }
+        public double Temperatur { get; set; }
+        public int Luftdruck { get; set; }  
+    }
+}
